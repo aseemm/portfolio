e4b74db2bab115e07810cd25a225f8bca26d3644
Aseem Maheshwari <aseemm@gmail.com>
2022-02-05 11:17:11 -0800

    mdx rendering

---
+++ src/pages/blog.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../components/layout";
+
+const BlogPage = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
+    </Layout>
+  );
+};
+
+// page query
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        body
+      }
+    }
+  }
+`;
+
+export default BlogPage;
